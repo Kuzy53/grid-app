1a69a652eb2fa87ea1b0624179767f63210e7516
Nikita Kuznetsov <nikitakuznetsov@MacBook-Pro-Nikita.local>
2024-12-09 17:15:03 +0300

    dev

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter
+  future= {{
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_startTransition: true,
+    }}>
+    <App />
+  </BrowserRouter>,
+);
